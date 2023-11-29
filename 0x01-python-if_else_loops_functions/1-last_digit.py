460ed1be5b77bbacacc68dd86ba1c402093f1af1
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-11-29 10:58:07 -0800

    1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,13 @@ import random
 number = random.randint(-10000, 10000)
 
 # Calculate the last digit of the number
-last_digit = abs(number) % 10 if abs(number) != 0 else 0
+last_digit = abs(number) % 10
+
+# Determine the sign of the last digit
+last_digit_sign = -1 if number < 0 else 1
 
 # Print the result
-print(f"Last digit of {number} is {last_digit}", end=" ")
+print(f"Last digit of {number} is {last_digit_sign * last_digit}", end=" ")
 
 # Determine if the last digit is greater than 5, 0, or less than 6 and not 0
 if last_digit > 5:
@@ -18,4 +21,3 @@ elif last_digit == 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
-

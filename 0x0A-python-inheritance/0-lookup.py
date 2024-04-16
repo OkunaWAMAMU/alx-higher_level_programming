732ab3bf91d243fd30b36ed841abff31c042da01
OkunaWAMAMU <vincentokuna1@gmail.com>
2024-04-16 11:58:37 -0700

    redoing

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
 
+"""Define an object attribute lookup function."""
+
 def lookup(obj):
-    """Returns a list of available attributes and methods of an object.
 
-    Args:
-        obj (object): The object to inspect.
+    """Return a list of an object's available attribute."""
 
-    Returns:
-        list: A list containing the names of attributes and methods.
-    """
-    # Get all attributes and methods of the object
-    attributes_and_methods = dir(obj)
-
-    # Filter out private attributes and methods
-    filtered_attributes_and_methods = [
-        item for item in attributes_and_methods if not item.startswith('__')
-    ]
-
-    return filtered_attributes_and_methods
+    return (dir(obj))

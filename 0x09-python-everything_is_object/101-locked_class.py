57ed5862db0074c258ec669803a84f11aca55ea1
OkunaWAMAMU <vincentokuna1@gmail.com>
2024-06-13 11:12:44 -0700

    me

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 class LockedClass:
-    __slots__ = ['first_name']
+    def __setattr__(self, name, value):
+        if hasattr(self, 'first_name') or name == 'first_name':
+            object.__setattr__(self, name, value)
+        else:
+            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))

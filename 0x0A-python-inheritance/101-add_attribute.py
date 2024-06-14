30c2c81f8ec22ce555127fc9ca9b778035e59b86
OkunaWAMAMU <vincentokuna1@gmail.com>
2024-06-14 08:09:42 -0700

    doing

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def add_attribute(obj, attr_name, attr_value):
     if hasattr(obj, '__dict__'):
         obj.__dict__[attr_name] = attr_value
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,16 +1,58 @@
+#!/usr/bin/python3
 class BaseGeometry:
+    """
+    Base class for geometry operations.
+    """
+
     def area(self):
-        raise Exception("area() is not implemented")
+        """
+        Calculate the area of the geometry.
+        Raises:
+            Exception: Always raises with the message 'area() is not implemented'.
+        """
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
+        """
+        Validate an integer value.
+
+        Args:
+            name (str): The name of the value being validated.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError('{} must be greater than 0'.format(name))
+
 
 class Rectangle(BaseGeometry):
+    """
+    Rectangle class, inherits from BaseGeometry.
+    """
+
     def __init__(self, width, height):
+        """
+        Initialize Rectangle with width and height.
+
+        Args:
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
+
+    def __str__(self):
+        """
+        Return a string representation of the Rectangle.
+
+        Returns:
+            str: String representation of the Rectangle.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)

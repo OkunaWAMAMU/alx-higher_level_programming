019ba4979544664b36c50d6d36992068cfcf20ad
OkunaWAMAMU <vincentokuna1@gmail.com>
2024-06-14 07:47:42 -0700

    doing

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,3 +1,24 @@
+#!/usr/bin/python3
+
 class BaseGeometry:
+    """
+    Base class for geometry operations.
+    """
+
     def area(self):
-        raise Exception("area() is not implemented")
+        """
+        Calculate the area of the geometry.
+        Raises:
+            Exception: Always raises with the message
+            'area() is not implemented'.
+        """
+        raise Exception('area() is not implemented')
+
+
+if __name__ == "__main__":
+    bg = BaseGeometry()
+
+    try:
+        print(bg.area())
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))

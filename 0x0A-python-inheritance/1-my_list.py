61421a35c9763b4d16a260c9b5154d9bb613a21f
OkunaWAMAMU <vincentokuna1@gmail.com>
2024-06-14 06:00:06 -0700

    read

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-
+"""
+This module contains a class MyList that inherits from list.
+"""
 
 class MyList(list):
-    """A class that inherits from list and provides a print_sorted method."""
+    """
+    A class that inherits from list and adds a print_sorted method.
+    """
 
     def print_sorted(self):
-        """Prints the list in sorted (ascending) order."""
-        print(sorted(self))
+        """
+        Prints the list in sorted (ascending) order.
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
